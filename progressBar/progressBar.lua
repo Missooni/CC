28f300b21258f6759db5cc687749fa6566ba8fc4
Miss ♥ <177053327+Missooni@users.noreply.github.com>
2024-11-22 23:39:04 -0500

    Update progressBar.lua

--- progressBar/progressBar.lua
+++ progressBar/progressBar.lua
@@ -1,29 +1,30 @@
--- Define or alias values using a table. I'm using strings for this example.
-local barTable = {"val1","val2","val3","val4","val5"}
+function gen(pTable, hidden)
+	local termWidth, termHeight = term.getSize()
+	local ogTerm = term.current()
+	local bgWindow = window.create(ogTerm, 1, 1, termWidth, termHeight, false)
 
-local termWidth, termHeight = term.getSize()
+	-- All customizable values.
+	local barWidth = math.floor(termWidth*.75)
+	local fullChar = " "
+	local emptyChar = "\143"
+	local bg = "f"
+	local fg = "0"
 
--- All customizable values.
-local barWidth = math.floor(termWidth*.75)
-local fullChar = " "
-local emptyChar = "\143"
-local bg = "f"
-local fg = "0"
-
--- Draws the empty progress bar.
-local mouseX, mouseY = term.getCursorPos()
-term.setCursorPos(termWidth/2-barWidth/2, mouseY)
-term.blit(emptyChar:rep(barWidth), bg:rep(barWidth), fg:rep(barWidth))
-
--- Iterates through the table and performs an action.
-for key, value in pairs(barTable) do
-	os.sleep(1)
-
--- Draws 'full characters' to the progress bar as the action finishes.
+	-- Draws the empty progress bar.
+	local mouseX, mouseY = term.getCursorPos()
 	term.setCursorPos(termWidth/2-barWidth/2, mouseY)
-	term.blit(fullChar:rep(math.floor(i / #table * barWidth)), bg:rep(math.floor(i / #table * barWidth)), fg:rep(math.floor(i / #table * barWidth)))
+	term.blit(emptyChar:rep(barWidth), bg:rep(barWidth), fg:rep(barWidth))
+
+	-- Iterates through the table and performs functions.
+	for key, value in pairs(pTable) do
+		if hidden then term.redirect(bgWindow) end
+		value()
+		term.redirect(ogTerm)
+	-- Draws 'full characters' to the progress bar as the action finishes.
+		term.setCursorPos(termWidth/2-barWidth/2, mouseY)
+		term.blit(fullChar:rep(math.floor(key / #pTable * barWidth)), bg:rep(math.floor(key / #pTable * barWidth)), fg:rep(math.floor(key / #pTable * barWidth)))
+	end
+	term.setCursorPos(mouseX, mouseY+1)
 end
 
-print(" Done!")
--- For this example, the program will end once any key is pressed.
-os.pullEvent( "key" )
+return gen
